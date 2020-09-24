a7d44e368e396e46f5d1c3190d1190c734878a87
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-09-24 08:54:23 +0300

    computes the RMS of the 0010 formalism. INCONSISTENCY in the actual rms value

--- Code/CPP/5params/signature_formalism/energies_0010.hh
+++ Code/CPP/5params/signature_formalism/energies_0010.hh
@@ -6,33 +6,62 @@
 class Bands_0010
 {
 public:
-    const double PI = 3.141592654;
-    const double spin0 = 6.5;
-    const double j_0 = 6.5;
-
     struct OmegaTuple
     {
         double omega1, omega2;
+        OmegaTuple()
+        {
+        }
+        OmegaTuple(double x)
+        {
+            omega1 = x;
+            omega2 = x;
+        }
     };
+
+    const double PI = 3.141592654;
+    const double spin0 = 6.5;
+    const double j_0 = 6.5;
+    const double error_checker = 6969;
+    const OmegaTuple FAILED_OMEGAS = OmegaTuple(error_checker);
+
+    int valid(double number)
+    {
+        if (!isnan(number) && number != error_checker)
+            return 1;
+        return 0;
+    }
 
     double b_term(double I, double j, double a1, double a2, double a3, double v, double gm)
     {
         auto t1 = ((2.0 * I - 1.0) * (a3 - a1) + 2.0 * j * a1) * ((2.0 * I - 1.0) * (a2 - a1) + 2.0 * j * a1) + 8.0 * a2 * a3 * I * j;
+        if (!valid(t1))
+            return error_checker;
         auto t2 = ((2.0 * j - 1.0) * (a3 - a1) + 2.0 * I * a1 + v * (2.0 * j - 1) / (j * (j + 1.0)) * sqrt(3.0) * (sqrt(3.0) * cos(gm) + sin(gm)));
+        if (!valid(t2))
+            return error_checker;
         auto t3 = ((2.0 * j - 1.0) * (a2 - a1) + 2.0 * I * a1 + v * (2.0 * j - 1.0) / (j * (j + 1.0)) * 2 * sqrt(3.0) * sin(gm));
+        if (!valid(t3))
+            return error_checker;
         return static_cast<double>((t1 + t2 * t3) * (-1.0));
     }
 
     double c_term(double I, double j, double a1, double a2, double a3, double v, double gm)
     {
         auto t1 = ((2.0 * I - 1.0) * (a3 - a1) + 2.0 * j * a1) * ((2.0 * j - 1.0) * (a3 - a1) + 2.0 * I * a1 + v * (2.0 * j - 1.0) / ((j * (j + 1))) * sqrt(3.0) * (sqrt(3.0) * cos(gm) + sin(gm))) - 4.0 * I * j * a3 * a3;
+        if (!valid(t1))
+            return error_checker;
         auto t2 = ((2.0 * I - 1.0) * (a2 - a1) + 2.0 * j * a1) * ((2.0 * j - 1.0) * (a2 - a1) + 2.0 * I * a1 + v * (2.0 * j - 1.0) / ((j * (j + 1))) * 2.0 * sqrt(3.0) * sin(gm)) - 4.0 * I * j * a2 * a2;
+        if (!valid(t2))
+            return error_checker;
         return static_cast<double>(t1 * t2);
     }
 
     double h_min(double I, double j, double a1, double a2, double a3, double v, double gm)
     {
         auto hmin = (a2 + a3) * (I + j) * 0.5 + a1 * pow(I - j, 2) - v * (2.0 * j - 1.0) / (j + 1) * sin(gm + (PI / 6));
+        if (!valid(hmin))
+            return error_checker;
         return static_cast<double>(hmin);
     }
 
@@ -40,7 +69,11 @@ public:
     {
         OmegaTuple result;
         auto B = b_term(I, j, a1, a2, a3, v, gm);
+        if (!valid(B))
+            return FAILED_OMEGAS;
         auto C = c_term(I, j, a1, a2, a3, v, gm);
+        if (!valid(C))
+            return FAILED_OMEGAS;
         result.omega1 = sqrt(0.5 * (-B - sqrt(B * B - 4.0 * C)));
         result.omega2 = sqrt(0.5 * (-B + sqrt(B * B - 4.0 * C)));
         return result;
@@ -49,21 +82,33 @@ public:
     double Energy(int n1, int n2, double I, double j, double a1, double a2, double a3, double v, double gm)
     {
         auto omega = Omega(I, j, a1, a2, a3, v, gm);
+        if (!valid(omega.omega1) && !valid(omega.omega2))
+            return error_checker;
         double E = h_min(I, j, a1, a2, a3, v, gm) + omega.omega1 * (n1 + 0.5) + omega.omega2 * (n2 + 0.5);
+        if (!valid(E))
+            return error_checker;
         return E;
     }
 
     double TSD1(double I, double a1, double a2, double a3, double v, double gm)
     {
         double E_0 = Energy(0, 0, spin0, j_0, a1, a2, a3, v, gm);
+        if (!valid(E_0))
+            return error_checker;
         double E = Energy(0, 0, I, j_0, a1, a2, a3, v, gm);
+        if (!valid(E))
+            return error_checker;
         return E - E_0;
     }
 
     double TSD2(double I, double a1, double a2, double a3, double v, double gm)
     {
         double E_0 = Energy(0, 0, spin0, j_0, a1, a2, a3, v, gm);
+        if (!valid(E_0))
+            return error_checker;
         double E = Energy(0, 0, I, j_0, a1, a2, a3, v, gm);
+        if (!valid(E))
+            return error_checker;
         return E - E_0;
     }
 
@@ -71,7 +116,11 @@ public:
     {
 
         double E_0 = Energy(0, 0, spin0, j_0, a1, a2, a3, v, gm);
+        if (!valid(E_0))
+            return error_checker;
         double E = Energy(1, 0, I - 1.0, j_0, a1, a2, a3, v, gm);
+        if (!valid(E))
+            return error_checker;
         return E - E_0;
     }
 
@@ -79,7 +128,11 @@ public:
     {
         const double j_2 = 4.5;
         double E_0 = Energy(0, 0, spin0, j_0, a1, a2, a3, v, gm);
+        if (!valid(E_0))
+            return error_checker;
         double E = Energy(0, 0, I, j_2, a1, a2, a3, v, gm);
+        if (!valid(E))
+            return error_checker;
         return E - E_0;
     }
 };
--- Code/CPP/5params/signature_formalism/main.cpp
+++ Code/CPP/5params/signature_formalism/main.cpp
@@ -1,28 +1,118 @@
 #include "energies_0010.hh"
 #include "energies_1230.hh"
 
-int main()
+const std::vector<double> spin1 = {8.5, 10.5, 12.5, 14.5, 16.5, 18.5, 20.5, 22.5, 24.5, 26.5, 28.5, 30.5, 32.5, 34.5, 36.5, 38.5, 40.5, 42.5, 44.5, 46.5, 48.5};
+const std::vector<double> spin2 = {13.5, 15.5, 17.5, 19.5, 21.5, 23.5, 25.5, 27.5, 29.5, 31.5, 33.5, 35.5, 37.5, 39.5, 41.5, 43.5, 45.5};
+const std::vector<double> spin3 = {16.5, 18.5, 20.5, 22.5, 24.5, 26.5, 28.5, 30.5, 32.5, 34.5, 36.5, 38.5, 40.5, 42.5};
+const std::vector<double> spin4 = {23.5, 25.5, 27.5, 29.5, 31.5, 33.5, 35.5, 37.5, 39.5, 41.5};
+const std::vector<double> tsd1 = {0.1966, 0.4597, 0.7746, 1.1609, 1.6112, 2.1265, 2.7051, 3.3441, 4.0411, 4.7937, 5.5992, 6.457, 7.3667, 8.3293, 9.3458, 10.4169, 11.5431, 12.7224, 13.9491, 15.2181, 16.5221};
+const std::vector<double> tsd2 = {1.3394, 1.7467, 2.2184, 2.7527, 3.3484, 4.003, 4.7143, 5.4805, 6.3004, 7.1733, 8.0998, 9.08, 10.1147, 11.2036, 12.3466, 13.5441, 14.7911};
+const std::vector<double> tsd3 = {2.1237, 2.6293, 3.1973, 3.8243, 4.5094, 5.2506, 6.0465, 6.8963, 7.7988, 8.7546, 9.7638, 10.8268, 11.9392, 13.0861};
+const std::vector<double> tsd4 = {4.58, 5.2251, 5.9273, 6.6819, 7.4919, 8.3573, 9.2778, 10.2535, 11.2851, 12.3701};
+const std::vector<int> gm = {15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+
+double rms(Bands_0010 &nucleus, double a1, double a2, double a3, double v)
 {
 
-    std::cout << "test the energies";
-    std::cout << "\n";
-    auto a1 = 1.0 / (2.0 * 73);
-    auto a2 = 1.0 / (2.0 * 3);
-    auto a3 = 1.0 / (2.0 * 67);
-    auto v = 6.01;
+    double best_rms = 9876543210.0;
+    double E;
+    double sum = 0.0;
+    double gamma = gm.at(6);
+    bool ok = 1;
+    int count = 0;
+    {
+        std::cout << "Started TSD1...";
+        std::cout << "\n";
+        for (auto id = 0; id < spin1.size() && ok; ++id)
+        {
+            E = nucleus.TSD1(spin1.at(id), a1, a2, a3, v, gamma * nucleus.PI / 180.0);
+            if (!nucleus.valid(E))
+            {
+                ok = 0;
+                std::cout << "Failed inside TSD1 at I=" << spin1.at(id);
+                std::cout << "\n";
+                break;
+            }
+            else
+            {
+                sum += pow(E - tsd1.at(id), 2);
+                count++;
+            }
+        }
+        std::cout << "Started TSD2...";
+        std::cout << "\n";
+        for (auto id = 0; id < spin2.size() && ok; ++id)
+        {
+            E = nucleus.TSD2(spin2.at(id), a1, a2, a3, v, gamma * nucleus.PI / 180.0);
+            if (!nucleus.valid(E))
+            {
+                ok = 0;
+                break;
+            }
+            else
+            {
+                sum += pow(E - tsd2.at(id), 2);
+                count++;
+            }
+        }
+        std::cout << "Started TSD3...";
+        std::cout << "\n";
+        for (auto id = 0; id < spin3.size() && ok; ++id)
+        {
+            E = nucleus.TSD3(spin3.at(id), a1, a2, a3, v, gamma * nucleus.PI / 180.0);
+            if (!nucleus.valid(E))
+            {
+                ok = 0;
+                break;
+            }
+            else
+            {
+                sum += pow(E - tsd3.at(id), 2);
+                count++;
+            }
+        }
+        std::cout << "Started TSD4...";
+        std::cout << "\n";
+        for (auto id = 0; id < spin4.size() && ok; ++id)
+        {
+            E = nucleus.TSD4(spin4.at(id), a1, a2, a3, v, gamma * nucleus.PI / 180.0);
+            if (!nucleus.valid(E))
+            {
+                ok = 0;
+                break;
+            }
+            else
+            {
+                sum += pow(E - tsd4.at(id), 2);
+                count++;
+            }
+        }
+    }
+    if (count == 62)
+        std::cout << sqrt(sum / 63.0) << "\n";
+    return count;
+}
 
+int main()
+{
+    //The bands initialization procedure
     Bands_0010 bands_0010;
-    Bands_1230 bands_1230;
 
-    auto gm = 21 * bands_0010.PI / 180.0;
-    std::vector<double> spins = {8.5, 10.5, 12.5, 14.5, 16.5, 18.5, 20.5, 22.5, 24.5, 26.5, 28.5, 30.5, 32.5, 34.5, 36.5, 38.5, 40.5, 42.5, 44.5, 46.5, 48.5};
-    for (auto &&n : spins)
-    {
-        std::cout << n << " " << bands_0010.TSD1(n, a1, a2, a3, v, gm) << " " << bands_0010.TSD2(n, a1, a2, a3, v, gm) << " " << bands_0010.TSD3(n, a1, a2, a3, v, gm) << " " << bands_0010.TSD4(n, a1, a2, a3, v, gm) << "\n";
-    }
-    for (auto &&n : spins)
-    {
-        std::cout << n << " " << bands_1230.TSD1(n, a1, a2, a3, v, gm) << " " << bands_1230.TSD2(n, a1, a2, a3, v, gm) << " " << bands_1230.TSD3(n, a1, a2, a3, v, gm) << " " << bands_1230.TSD4(n, a1, a2, a3, v, gm) << "\n";
-    }
+    //The constants used in computations (testing purposes only)
+    auto a1 = 1.0 / (2.0 * 73.0);
+    auto a2 = 1.0 / (2.0 * 3.0);
+    auto a3 = 1.0 / (2.0 * 67.0);
+    auto v = 6.01;
+    // auto gm = 21.0 * bands_0010.PI / 180.0;
+
+    std::cout << rms(bands_0010, a1, a2, a3, v);
+    // for (auto &&n : spin1)
+    // {
+    //     std::cout << n << " " << bands_0010.TSD1(n, a1, a2, a3, v, gm) << " " << bands_0010.TSD2(n, a1, a2, a3, v, gm) << " " << bands_0010.TSD3(n, a1, a2, a3, v, gm) << " " << bands_0010.TSD4(n, a1, a2, a3, v, gm) << "\n";
+    // }
+    // for (auto &&n : spins)
+    // {
+    //     std::cout << n << " " << bands_1230.TSD1(n, a1, a2, a3, v, gm) << " " << bands_1230.TSD2(n, a1, a2, a3, v, gm) << " " << bands_1230.TSD3(n, a1, a2, a3, v, gm) << " " << bands_1230.TSD4(n, a1, a2, a3, v, gm) << "\n";
+    // }
     std::cout << "\n";
 }
