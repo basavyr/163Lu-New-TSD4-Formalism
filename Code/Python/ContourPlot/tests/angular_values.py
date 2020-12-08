2ab9851d9e31ac3863486d280f4e428306070116
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-08 18:01:17 +0200

    adds simplifed H into the output files for a given spin and fixed coordinates

--- Code/Python/ContourPlot/tests/angular_values.py
+++ Code/Python/ContourPlot/tests/angular_values.py
@@ -4,6 +4,11 @@ sys.path.append('../')
 
 import contour as cp
 
+def H(spin,q):
+	CT=cp.CT
+	ret_val=cp.SimplifiedH(spin,CT,q)
+	return ret_val
+
 def TestCoordinates():
 	q=cp.mt
 	l_theta=len(cp.thetas)
@@ -12,19 +17,20 @@ def TestCoordinates():
 		#print(q[id])
 		id=id+10000
 
-def TestThetaEvolution(varphi):
+def TestThetaEvolution(spin,varphi):
 	ret_list=[]
 	for id in range(len(cp.thetas)):
 		if(id % 10 ==0):
-			q=(cp.thetas[id],varphi)
+			q=(cp.thetas[id],varphi,H(spin,[cp.thetas[id],varphi]))
 #			print(cp.thetas[id],varphi)
 			ret_list.append(q)
 	return ret_list		
-def TestPhiEvolution(theta):
+def TestPhiEvolution(spin,theta):
 	ret_list=[]
 	for id in range(len(cp.phis)):
 		if(id % 10 ==0):
-			q=(theta,cp.phis[id])
+			# q=(theta,cp.phis[id])
+			q=(theta,cp.phis[id],H(spin,[theta,cp.phis[id]]))
 #			print(theta,cp.phis[id])
 			ret_list.append(q)
 	return ret_list		
@@ -36,14 +42,13 @@ varphi_file='phi_evolution.dat'
 def WriteData(out_file,data):
 	f=open(out_file,'w')
 	for x_id in data:
-		f.write(str(x_id[0])+' , '+str(x_id[1]))
+		f.write(str(x_id[0])+' , '+str(x_id[1])+' , '+str(x_id[2]))
 		f.write('\n')
 	f.close()
-	
-WriteData(theta_file,TestThetaEvolution(cp.phis[1]))
-WriteData(varphi_file,TestPhiEvolution(cp.thetas[1]))
 
-print(cp.SimplifiedH(8.5,cp.CT,[cp.thetas[1],cp.phis[1]]))
+I=8.5	
+WriteData(theta_file,TestThetaEvolution(I,cp.phis[1]))
+WriteData(varphi_file,TestPhiEvolution(I,cp.thetas[1]))
 
 # Opening the test files for saving the data
 # f1=open(theta_file,'w')
