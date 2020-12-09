f62b3082d4174100e94c43d3a146e56f0c468016
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-09 14:45:06 +0200

    plot comparison of H between mathematica and python using constant theta and varphi

--- Code/Python/ContourPlot/tests/plot_tables.py
+++ Code/Python/ContourPlot/tests/plot_tables.py
@@ -2,17 +2,33 @@ import matplotlib.pyplot as plt
 import import_tables as imp
 import numpy as np
 
+path_to_storage="../../../../Resources/Output_Data/ContourPlots/"
 
-values_to_compare=list(zip(imp.py_phi_values,imp.math_varphi_values))
+theta_compare=list(zip(imp.py_phi_values,imp.math_varphi_values))
+varphi_compare=list(zip(imp.py_theta_values,imp.math_theta_values))
 #for x in values_to_compare:
 #	print(x[0],x[1])
 
 
-y1=[x[0] for x in values_to_compare]
-y2=[x[1] for x in values_to_compare]
-# print(y1)
-# print(y2)
+theta_image_path=path_to_storage+'theta.pdf'
+varphi_image_path=path_to_storage+'varphi.pdf'
+
+#plot the evolution of H with varphi 
+y1=[x[0] for x in theta_compare]
+y2=[x[1] for x in theta_compare]
+
 plt.plot(y1,'-r',label='python')
 plt.plot(y2,'ob',label='math')
 plt.legend(loc='best')
-plt.savefig('plot.pdf',bbox_inches='tight',dpi=300)
+plt.savefig(theta_image_path,bbox_inches='tight',dpi=300)
+plt.close()
+#plot the evolution of H with theta
+
+w1=[x[0] for x in varphi_compare]
+w2=[x[1] for x in varphi_compare]
+
+plt.plot(w1,'-r',label='python')
+plt.plot(w2,'ob',label='math')
+plt.legend(loc='best')
+plt.savefig(varphi_image_path,bbox_inches='tight',dpi=300)
+plt.close()
