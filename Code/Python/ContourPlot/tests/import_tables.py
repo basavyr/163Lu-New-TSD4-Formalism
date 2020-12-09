c036b0a1b23dec12dcef2c8ae0f0f0631443b913
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-09 13:33:38 +0200

    math doc now exports the data into proper location. fixed imported array structure for both py and math

--- Code/Python/ContourPlot/tests/import_tables.py
+++ Code/Python/ContourPlot/tests/import_tables.py
@@ -1,5 +1,6 @@
 # declare the import paths for math and py files
 import angular_values as av
+import matplotlib.pyplot as plt
 
 def ImportFunction(file_path):
 	table=open(file_path,'r')
@@ -12,5 +13,19 @@ def ImportFunction(file_path):
 		ret_val.append(current_h[2])
 	return ret_val
 
-fct=ImportFunction(av.py_theta_file)
-print(fct)
+def MathematicaImport(file_path):
+	table=open(file_path,'r')
+	info=table.readlines()
+	convert=lambda x: float(x)
+	ret_val=[]
+	for line in info:
+		values=line.split()
+		current_h=list(map(convert,values))
+		ret_val.append(current_h[2])
+	return ret_val 
+
+#theta evolution
+py_theta_values=ImportFunction(av.py_theta_file)
+math_theta_values=MathematicaImport(av.math_theta_file)
+print(len(py_theta_values))
+print(len(math_theta_values))
