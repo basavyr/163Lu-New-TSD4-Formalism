9878dc4369e518351a7c9315c74d32f7cb5f0adc
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-12-09 14:22:45 +0200

    adds proper plot for contour comparison

--- Code/Python/ContourPlot/tests/plot_tables.py
+++ Code/Python/ContourPlot/tests/plot_tables.py
@@ -1,2 +1,18 @@
 import matplotlib.pyplot as plt
 import import_tables as imp
+import numpy as np
+
+
+values_to_compare=list(zip(imp.py_phi_values,imp.math_varphi_values))
+#for x in values_to_compare:
+#	print(x[0],x[1])
+
+
+y1=[x[0] for x in values_to_compare]
+y2=[x[1] for x in values_to_compare]
+# print(y1)
+# print(y2)
+plt.plot(y1,'-r',label='python')
+plt.plot(y2,'ob',label='math')
+plt.legend(loc='best')
+plt.savefig('plot.pdf',bbox_inches='tight',dpi=300)
